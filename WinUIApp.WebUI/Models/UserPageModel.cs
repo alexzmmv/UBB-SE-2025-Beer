d3971988e15f96a8605f13b6c71fcda63a9427a2
Florescu Daria <115116346+daria-florescu@users.noreply.github.com>
2025-05-31 23:05:55 +0300

    added UserPageModel

--- WinUIApp.WebUI/Models/UserPageModel.cs
+++ WinUIApp.WebUI/Models/UserPageModel.cs
@@ -1,13 +1,13 @@
 ﻿using DataAccess.Model.AdminDashboard;
-using DataAccess.Model.Authentication;
 using WinUiApp.Data.Data;
 
 namespace WebServer.Models
 {
     public class UserPageModel
     {
-        public required User CurrentUser { get; set; }
-        public required IEnumerable<DataAccess.DTOModels.ReviewDTO> CurrentUserReviews { get; set; }
-        public required IEnumerable<string> CurrentUserDrinks { get; set; }
+        public User CurrentUser { get; set; }
+        public IEnumerable<Review> CurrentUserReviews { get; set; }
+        public IEnumerable<string> CurrentUserDrinks { get; set; }
+        public bool HasPendingUpgradeRequest { get; set; }
     }
 }
