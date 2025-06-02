4d81bebf6f782df864634af966ab03a07b41ab11
Bianca Cîrnu <bianca.georgiana.cirnu@gmail.com>
2025-06-02 05:20:36 +0300

    filters from home page work

--- WinUIApp.WebUI/Models/HomeViewModel.cs
+++ WinUIApp.WebUI/Models/HomeViewModel.cs
@@ -9,4 +9,10 @@ public class HomeViewModel
     public List<Category> drinkCategories { get; set; }
     public List<Brand> drinkBrands { get; set; }
     public List<DrinkDTO> drinks { get; set; }
+    public string SearchKeyword { get; set; }
+    public float? MinValue { get; set; }
+    public float? MaxValue { get; set; }
+    public int? MinStars { get; set; }
+    public string[] SelectedCategories { get; set; }
+    public string[] SelectedBrandNames { get; set; }
 }
